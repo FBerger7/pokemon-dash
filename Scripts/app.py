f59c5ed7d5c59ea4666612a8a5ad182ab14eece4
KingBaum <bergerfranek@gmail.com>
2020-12-21 02:18:23 +0100

    .gitignore fix

---
+++ Scripts/app.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+import pygame
+
+from Scripts.ethan import Ethan
+from config import SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+FPS = 60
+
+
+class App:
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.width, self.height = SCREEN_WIDTH, SCREEN_HEIGHT
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption(GAME_TITLE)
+
+        self.ethan = None
+
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
+        self.ethan = Ethan()
+        # self.all_sprites.add(self.ethan)
+
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_RIGHT:
+        #         self.ethan.go_right()
+        #     if event.key == pygame.K_LEFT:
+        #         self.ethan.go_left()
+        #     if event.key == pygame.K_UP:
+        #         self.ethan.go_up()
+        #     if event.key == pygame.K_DOWN:
+        #         self.ethan.go_down()
+
+    def on_loop(self):
+        keys = pygame.key.get_pressed()
+        if keys:
+            if keys[pygame.K_RIGHT]:
+                self.ethan.go_right()
+            if keys[pygame.K_LEFT]:
+                self.ethan.go_left()
+            if keys[pygame.K_UP]:
+                self.ethan.go_up()
+            if keys[pygame.K_DOWN]:
+                self.ethan.go_down()
+
+    def on_render(self):
+        self._display_surf.fill(BLACK)
+        self._display_surf.blit(self.ethan.sprite, (self.ethan.posx, self.ethan.posy))
+
+    def on_cleanup(self):
+        pygame.quit()
+
+    def on_execute(self):
+        self.on_init()
+
+        while self._running:
+            self.clock.tick(FPS)
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+            pygame.display.flip()
+        self.on_cleanup()
---
+++ Scripts/ethan.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import os
+
+import pygame
+
+from Scripts import ETHAN_STANDING, ETHAN_RIGHT, ETHAN_LEFT, ETHAN_UP
+
+
+class Ethan(pygame.sprite.Sprite):
+    """
+    Class representing the main character
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.image = self._load_image(ETHAN_STANDING)
+        self.size = self.image.get_size()
+        self.sprite = pygame.transform.scale(self.image, (int(self.size[0] * 2.15), int(self.size[1] * 2.15)))
+        self.posx = 100
+        self.posy = 100
+        self.step = 5
+
+    @staticmethod
+    def _load_image(file_path):
+        try:
+            image = pygame.image.load(file_path)
+        except pygame.error as message:
+            raise ValueError('Cannot load image: ', os.path.basename(file_path))
+
+        return image
+
+    def _load_new_sprite(self, sprite_path):
+        self.image = self._load_image(sprite_path)
+        self.size = self.image.get_size()
+        self.sprite = pygame.transform.scale(self.image, (int(self.size[0] * 2.15), int(self.size[1] * 2.15)))
+
+    def go_right(self):
+        self._load_new_sprite(ETHAN_RIGHT)
+        self.posx += self.step
+
+    def go_left(self):
+        self._load_new_sprite(ETHAN_LEFT)
+        self.posx -= self.step
+
+    def go_up(self):
+        self._load_new_sprite(ETHAN_UP)
+        self.posy -= self.step
+
+    def go_down(self):
+        self._load_new_sprite(ETHAN_STANDING)
+        self.posy += self.step
